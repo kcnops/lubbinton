9c3c5b069e616ce45aa6891ddc7123f13d185319
Kristof Cnops <kristof.cnops@gmail.com>
2018-12-18 23:38:39 +0100

    extended testapplication to increase amount of players and test until breakpoint

--- src/main/java/kcnops/lubbinton/TestApplication.java
+++ src/main/java/kcnops/lubbinton/TestApplication.java
@@ -6,29 +6,41 @@ import kcnops.lubbinton.service.incrementalDistributor.IIncrementalDistributor;
 import kcnops.lubbinton.service.incrementalDistributor.IncrementalDistributor;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class TestApplication {
 
 //	private static final String[] NAMES = new String[]{"Kristof","Thomas","Lucas", "Smets", "Geert"};
-//	private static final String[] NAMES = new String[]{"Kristof","Thomas","Lucas", "Smets", "Geert", "Bart", "Arno", "Yannick"};
-	private static final String[] NAMES = new String[]{"Kristof","Thomas","Lucas", "Smets", "Geert", "Bart", "Arno", "Yannick", "Thierry", "Shauny", "Julie"};
-	private static final int AMOUNT_OF_ROUNDS = 10;
+//	private static final String[] NAMES = new String[]{"Kristof","Thomas","Lucas", "Smets", "Geert", "Bart", "Arno", "Yannick", "Thierry", "Shauny"}; // OK 37sec
+//	private static final String[] NAMES = new String[]{"Kristof","Thomas","Lucas", "Smets", "Geert", "Bart", "Arno", "Yannick", "Thierry", "Shauny", "Julie"}; // NOK
+	private static final String[] NAMES = new String[]{"Kristof","Thomas","Lucas", "Smets", "Geert", "Bart", "Arno", "Yannick", "Thierry", "Shauny", "Julie", "Esther"}; // NOK
+	private static final int AMOUNT_OF_ROUNDS = 5;
 
 	private static final IIncrementalDistributor DISTRIBUTOR = new IncrementalDistributor();
 
 	public static void main(String[] args) {
-		final List<Player> players = Arrays.stream(NAMES).map(Player::new).collect(Collectors.toList());
-		final List<Round> rounds = new ArrayList<>();
+		final List<Player> players = new ArrayList<>();
+		boolean stop = false;
+		for (int amountOfPlayers = 1; !stop;  amountOfPlayers++) {
+			final Player newPlayer = new Player("player" + amountOfPlayers);
+			players.add(newPlayer);
 
-		for (int i = 0; i < AMOUNT_OF_ROUNDS; i++) {
-			final Round nextRound = DISTRIBUTOR.getNextRound(players, rounds);
-			System.out.println(nextRound);
-			rounds.add(nextRound);
+			System.out.println("Calculating for " + amountOfPlayers + " players...");
+
+			final List<Round> rounds = new ArrayList<>();
+			for (int i = 0; i < AMOUNT_OF_ROUNDS; i++) {
+				long startTime = System.currentTimeMillis();
+				rounds.add(DISTRIBUTOR.getNextRound(players, rounds));
+				long endTime = System.currentTimeMillis();
+				double duration = (endTime - startTime)/1000d;
+				System.out.println("Time: " + duration);
+				if(duration > 60) {
+					System.out.println("Stopping as last duration was > 60, that's too damn long!");
+					stop = true;
+					break;
+				}
+			}
 		}
-
 	}
 
 }
